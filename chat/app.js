99ce654f532e3d801bd9a55cfe564976d38a9a77
mher3217 <mher-harutyunyan92@mail.ru>
2018-10-08 16:42:56 +0400

    Update app.js

--- chat/app.js
+++ chat/app.js
@@ -1,6 +1,8 @@
 const WebSocket = require('ws');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const app = require('express')();
+
 
 const chatDB = mongoose.createConnection('mongodb://localhost:27017/chat');
 
@@ -19,39 +21,74 @@ let db = {
 };
 
 
-const wss = new WebSocket.Server({ port: 8080 });
+//const wss = new WebSocket.Server({ port: 8080 });
 
-let clients = [];
+app.get('/', function (req, res) {
+   res.sendfile(__dirname + '/index.html');
+})
+app.listen(3000, function () {
+   console.log('Example app listening on port 3000!')
+})
 
-wss.on('connection', function connection(ws) {
+var WebSocketServer = require('ws').Server,
+  wss = new WebSocketServer({port: 8080})
 
-  clients.push(ws);
-  ws.send('\n1. authorize\n2.choose channels\n3.write message');
 
-  ws.on('message', function incoming(message) {
-    if(message == '1'){
-      ws.send('insert name:' );
-        username = message;
-        users.createMany({
-          username: db.users.username,
-          age: db.users.age,
-          time:db.message.date,
-        });
-      // ws.send('insert age:');
+wss.on('connection', function (ws) {
+  ws.on('message', function (message) {
+
+
+      console.log('received: %s', message);
+
+
+
+
+    // if(db.users.find({username: user.username}),((err, result)=> {
+    //          if (err) throw err;
+    //          console.log(result);
+    //      })){
+    //
     // }
-    }else if (message == '2') {
-      //channels
-    }else {
-        (clients || []).forEach((c, i) => {
-          if(c == ws){
-            return;
-          }
-          c.send('User:' + i + '.Message-- ' + message);
-        })
-    }
+
+
+
   });
 });
 
+// wss.on('connection', function connection(ws) {
+//
+//   clients.push(ws);
+//   ws.send('\n1. authorize\n2.login\n3.write message');
+//
+//   ws.on('message', function incoming(message) {
+//     if(message == '1'){
+//
+//       ws.send('insert name:' + 'insert age:');
+//           while(met.username == null){
+//             met.username = message;
+//             console.log(met.username);
+//           }
+//           while(met.username == '1'){
+//             met.age = message;
+//             console.log(met.age);
+//           }
+//     }else if (message == '2') {
+//       db.users.find(/*{username: "ator"},*/(function(err, result) {
+//        if (err) throw err;
+//        console.log(result);
+//      }));
+//       //channels
+//     }else {
+//         (clients || []).forEach((c, i) => {
+//           if(c == ws){
+//             return;
+//           }
+//           c.send('User:' + i + '.Message-- ' + message);
+//         })
+//     }
+//   });
+//
+// });
 
 // ws.send (JSON.stringify ({
 //   type: 'message',
