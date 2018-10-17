19d38b631a0e4f1505c0bc940c44d612220baea3
mher3217 <mher-harutyunyan92@mail.ru>
2018-10-17 11:27:49 +0400

    Update service.js

--- books/services/users/service.js
+++ books/services/users/service.js
@@ -1,1 +1,27 @@
 const mongoose = require('mongoose');
+
+class UsersService {
+  constructor(){
+    const libery = mongoose.createConnection('mongodb://localhost:27017/libery');
+    let usersSchema = require('./model.js');
+    let booksSchema =  require('./model.js');
+    this.db = {
+      users: libery.model('users', usersSchema),
+      books: libery.model('books', booksSchema),
+    }
+  }
+
+  getUsers(){
+    return new Promise((resolve, reject)=>{
+      this.db.books.find().then(books =>{
+        console.log(books);
+        resolve(books);
+      }).catch(err =>{
+        reject(err);
+      });
+    });
+
+  }
+}
+
+module.exports = new UsersService();
