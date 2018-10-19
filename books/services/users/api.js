afc5076fe6e9aa9344caa133b08b933ce45397e5
Mher3217 <mher-harutyunyan92@mail.ru>
2018-10-19 13:51:41 +0400

    libery_new

--- books/services/users/api.js
+++ books/services/users/api.js
@@ -2,7 +2,6 @@ const express = require('express');
 const UsersRouter = express.Router();
 const UsersService = require('./service.js');
 
-<<<<<<< HEAD
 var optional = {};
 
 ///////////////////////////////
@@ -135,64 +134,6 @@ UsersRouter.delete('/user_delete/:key', (req,res)=>{
     console.log(err);
     return res.send('error')
   })
-=======
-UsersRouter.get('/books', (req, res)=> {
-  let p = UsersService.getUsers();
-  p.then(books =>{
-    return res.send(books);
-  }).catch(err =>{
-    return res.send('error')
-  })
-    // db.books.find({title: new RegExp('^' + (req.query.q || ''),'gi')},
-    //   (err, books) =>{
-    //     if(err || !books){
-    //       return res.send('books_error');
-    //     }
-    //     return res.send(books);
-    // });
-})
-UsersRouter.get('/books/:id', (req,res)=>{
-  db.books.findOne({_id: req.params.id}, (err, books) =>{
-      if(err || ! books){
-        return res.send('book not found');
-      }
-      return res.send(books);
-    })
-});
-
-UsersRouter.post('/new_book', (req, res)=>{
-  db.books.create({
-    title: req.body.title,
-    pages: req.body.pages,
-    occupied: false
-  }, function(err, books){
-      if(err || !books){
-        return res.send('books_error!!!!!');
-      }
-      return res.send('books_post_ok!!!');
-  })
-});
-
-UsersRouter.put('/book_put/:name', (req,res)=>{
-  db.books.updateOne({title: req.params.name},{
-    title: req.body.title,
-    pages: req.body.pages
-  }, function(err,book){
-      if(err || !book){
-        return res.send('books_put error!!!!!');
-      }
-      return res.send('books_put_ok!!!');
-  })
-})
-UsersRouter.delete('/book_delete/:key', (req,res)=>{
-  db.books.deleteOne({key: req.params.key},
-    function(err,book){
-      if(err | !book){
-        return res.send('book_delete error!!!!!');
-      }
-      return res.send('book_delete!!!');
-    });
->>>>>>> 5658a1c8768a17ae5302928695f2bf1eb7c9ecea
 })
 
 module.exports = UsersRouter;
--- books/services/users/model.js
+++ books/services/users/model.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 const shortid = require('shortid');
 
@@ -41,48 +40,3 @@ module.exports = {
       }
     }),
 }
-=======
-const mongoose = require('mongoose');
-const shortid = require('shortid');
-
-function generateAPIKey(){
-  return shortid.generate() + '_' + shortid.generate() + '_' + shortid.generate();
-}
-
-const usersSchema = new mongoose.Schema({
-  key: {
-    type: String,
-    default: generateAPIKey()
-  },
-  username: {
-    type:String,
-    lowercase:true,
-    trim:true,
-    index:true,
-    require:true
-  },
-  have_book:{
-    type: []
-  }
-});
-
-
-const booksSchema = new mongoose.Schema({
-  key: {
-    type: String,
-    default: generateAPIKey()
-  },
-  title: {
-    type:String,
-    lowercase:true,
-  },
-  occupied: Boolean,
-  pages: Number,
-  occupied_by: {
-    obj: String,//{type: Schema.Types.ObjectId, ref: 'users'},
-  }
-});
-
-module.exports = booksSchema;
-module.exports = usersSchema;
->>>>>>> 5658a1c8768a17ae5302928695f2bf1eb7c9ecea
